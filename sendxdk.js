93b94e3ad1b2b3e9db0d28818cdf2b1521016b53
cjm1203777 <34873398+cjm1203777@users.noreply.github.com>
2017-12-28 08:19:25 +0900

    d

--- sendxdk.js
+++ sendxdk.js
@@ -1,40 +1,37 @@
-
-
 injector = angular.element(document.body).injector();
-    $http = injector.get("$http");
-    function abc() {
-        var i2n = function (imin) {
-            var c = 0, aa = 0, st,bb;
-            var link = "https://orbi.kr/api/v1/user/" + imin + "/timeline";
-            var bc = $http({
-                method: 'GET',
-                url: link,
-                transformResponse: function (data) {
-                    return angular.fromJson(data)
-                }
-            });
-            bc.then(function (response) {
-                bd = response.data['data']['posts'][0]['id'];
-                bb = '000' + bd.toString();
-            });
-            return bb;
-        };
-        var abh =  function (num, r) {
-            if (num === undefined) {
-                return "there is an error";
-            }
-            var link2 = "https://orbi.kr/api/v1/board/post/" + num + "/send_xdk";
-            var bj = $http({
-                method: 'POST',
-                url: link2,
-                data: {value: r},
-                transformResponse: function (data) {
-                    return angular.fromJson(data)
-                }
-            });
-            bj.then(function (response) {
-                var bk = response.data['data']['trx'];
-                alert('거래 번호 : ' + bk);
-            });
-        };
+$http = injector.get("$http");
+var i2n = function (imin) {
+    var c = 0, aa = 0, st,bb;
+    var link = "https://orbi.kr/api/v1/user/" + imin + "/timeline";
+    var bc = $http({
+        method: 'GET',
+        url: link,
+        transformResponse: function (data) {
+            return angular.fromJson(data)
+        }
+    });
+    bc.then(function (response) {
+        bd = response.data['data']['posts'][0]['id'];
+        bb = '000' + bd.toString();
+    });
+    return bb;
+};
+var abh =  function (num, r) {
+    if (num === undefined) {
+        return "there is n error";
     }
+    var link2 = "https://orbi.kr/api/v1/board/post/" + num + "/send_xdk";
+    var bj = $http({
+        method: 'POST',
+        url: link2,
+        data: {value: r},
+        transformResponse: function (data) {
+            return angular.fromJson(data)
+        }
+    });
+    bj.then(function (response) {
+        var bk = response.data['data']['trx'];
+        alert('거래 번호 : ' + bk);
+    });
+};
+
